c4504f9074c6dbe0721ac5e5936d0ebadeee2d22
emeraldhycient <igwezehycient86@gmail.com>
2023-04-30 13:15:40 +0100

    [update] adjusted the cards

--- src/component/home/avatarCard.jsx
+++ src/component/home/avatarCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function AvatarCard() {
   return (
-    <div className='rounded-lg flex w-[250px] bg-blue-200 h-[60px] items-center justify-around m-4 shadow'>
+    <div className="w-[250px] h-[60px] bg-amber-300 shadow flex items-center justify-around rounded-lg m-4">
       <h5>12</h5>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png" alt="" />
-      <h4>Kakuna</h4>
+      <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png' />
+      <h4>kakuna</h4>
     </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,20 @@
-import React from "react";
-import AvatarCard from "../component/home/avatarCard";
-import Avatar from "../component/home/avatar";
+import React from 'react'
+import AvatarCard from '../component/home/avatarCard'
 
 function Home() {
   return (
     <section>
-      <div className="grid h-screen grid-cols-5 w-full">
-        <div className="col-span-3 h-full grid grid-cols-2 p-8">
-          <AvatarCard/>
-          <AvatarCard/>
-          <AvatarCard/>
-          <AvatarCard/>
+      <div className="grid grid-cols-5 w-full">
+        <div className="col-span-3 grid grid-cols-2 p-8">
+          <AvatarCard />
+          <AvatarCard />
+          <AvatarCard />
+          <AvatarCard />
         </div>
-        <div className="h-full col-span-2  ">
-        <Avatar/>
-        </div>
+        <div className="col-span-2"></div>
       </div>
     </section>
-  );
+  )
 }
 
-export default Home;
+export default Home
