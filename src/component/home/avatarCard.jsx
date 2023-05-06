1ec416e6a0492b3b261b1f601369f9eec2cb132a
Ibok Patience <ibok@uridiumtechnologies.com>
2023-05-06 14:34:28 +0100

    adding api

--- src/component/home/avatarCard.jsx
+++ src/component/home/avatarCard.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function AvatarCard() {
+function AvatarCard({ fighter }) {
+  const [Card, setCard] = useState({});
+  const [On, setOn] = useState(false);
+
+  const getAvatars = async () => {
+    try {
+      const response = await axios.get(fighter.url);
+      console.log(response.data);
+      setCard(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useState(() => getAvatars(), []);
+
   return (
-    <div className='rounded-lg flex w-[250px] bg-blue-200 h-[60px] items-center justify-around m-4 shadow'>
-      <h5>12</h5>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png" alt="" />
-      <h4>Kakuna</h4>
-    </div>
-  )
+    <section>
+      <div
+        onClick={() => setOn(!On)}
+        className="rounded-lg flex w-[250px] bg-blue-200 h-[60px] items-center justify-around m-4 shadow"
+      >
+        <h5>{Card?.id}</h5>
+        <img src={`${Card?.sprites?.front_shiny}`} alt="" />
+        <h4>{fighter?.name}</h4>
+      </div>
+      {On ? (
+        <center>
+        <div>
+          <img className="h-[100px]" src={`${Card?.sprites?.front_shiny}`} alt="" /> 
+          {
+            Card?.stats?.map((item,index)=>(
+              <div key={index} className="flex items-center justify-between  w-[200px]">
+              <h5>{item?.stat?.name}</h5>
+              <h5>{item?.base_stat}</h5>
+            </div>
+            ))
+          }
+       
+        </div>
+        </center>
+      ) : null}
+    </section>
+  );
 }
 
-export default AvatarCard
+export default AvatarCard;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import axios from "axios";
 import AvatarCard from "../component/home/avatarCard";
 import Avatar from "../component/home/avatar";
+import Header from "../component/common/header";
+
 
 function Home() {
+  const [avatar,setAvatar] = useState({});
+
+  const getAvatars = async () => {
+    try {
+      const req = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+      // console.log(req.data);
+      setAvatar(req.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  {
+  }
+
+   useEffect(() => {
+    getAvatars();
+  }, []);
+
   return (
     <section>
-      <div className="grid h-screen grid-cols-5 w-full">
-        <div className="col-span-3 h-full grid grid-cols-2 p-8">
-          <AvatarCard/>
-          <AvatarCard/>
-          <AvatarCard/>
-          <AvatarCard/>
-        </div>
-        <div className="h-full col-span-2  ">
-        <Avatar/>
-        </div>
+      <Header />
+      <div className="w-10/12 md:w-8/12 lg:w-6/12 mx-auto grid md:grid-cols-2 grid-cols-1">
+       {
+        avatar?.results?.map((avatar,index) => (
+          <AvatarCard fighter={avatar} key={index}/>
+        ))
+       }
       </div>
     </section>
   );
